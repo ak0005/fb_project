8fabbc18a2c17c7f16a57d3363409cabf0ba9097
ak0005 <46166186+ak0005@users.noreply.github.com>
2021-02-11 19:02:00 +0530

    removed my access token
    create your own access token and make sure to extend its lifetime

--- const.py
+++ const.py
@@ -2,7 +2,7 @@
 #user_managed_groups, groups_show_list, groups_access_member_info, public_profile
 #https://developers.facebook.com/tools/debug/accesstoken/
 
-token='EAAJKCn7sDoYBAHuPOXwNGyRUReCofs4bPVOcfyGrU7ayD6Mybx3uunVic1CJu47E7NCYmF7oTuGqcVwGgZC4jOhbUYvOa3L0cVP0ULoU2PmDNd0CEdf28UFxkoHu1Ftk61Ab2CIeSFr0ZAKpW8baMVQ0Bv8GZByZAjdpJNlqLoHwitcZBhtj0HX6styiDrQFLk49lxG6ZAl7eQmBDhBgQJIi87vLCU7CR5NoISg7zsSAZDZD'
+token='access token'
 grpId='161136235596016'
 
 PostCollection='post'
