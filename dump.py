16521684b35889fb3a2d53f3b05b20cc8e1c22b0
ak0005 <akhilshukla2000@gmail.com>
2021-03-09 16:54:39 +0530

    smaller_code

--- dump.py
+++ dump.py
@@ -2,21 +2,6 @@ import os
 import const as const
 
 os.system('mongoexport -d '+const.grpId+' -c ' +
-          const.PostCollection+' -o '+const.dump)
-fp = open(const.dump, 'r')
-arr = fp.readlines()
-fp.close()
-
-fp = open(const.dump, 'w')
-
-fp.write("{\"posts\":[\n")
-
-for i in range(len(arr)-1):
-    fp.write(arr[i][:-1]+',\n')
-
-fp.write(arr[-1])
-fp.write("]\n}")
-
-fp.close()
+          const.PostCollection+' -o '+const.dump+' --pretty')
 
 os.system('mongodump --db '+const.grpId)
